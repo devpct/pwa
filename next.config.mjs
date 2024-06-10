404b2b5cee613f6da117e1d4cbf13246a8fcc25b
devpct <mr.mohammad.pg19@gmail.com>
2024-06-10 22:35:08 +0330

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ import withPWA from 'next-pwa';
 
 const config = withPWA({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
+
 });
 
 export default config;
